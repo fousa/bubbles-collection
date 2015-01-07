e7a2ed82178f9df0f72777f84cd5bf7c80bc21bd
Jelle Vandebeeck <jelle.vandebeeck@icapps.com>
2015-01-07 09:10:04 +0100

    Add basic collection view.

--- Bubbles Collection/ViewController.swift
+++ Bubbles Collection/ViewController.swift
@@ -8,7 +8,19 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UICollectionViewDataSource, UICollectionViewDelegate {
+    
+    // MARK: - Outlets
+    
+    @IBOutlet var collectionView: UICollectionView!
+    
+    // MARK: - Initialization
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    // MARK: - View Flow
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,7 +31,23 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    // MARK: - UICollectionView
+    
+    func collectionView(collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(collectionView: UICollectionView, cellForItemAtIndexPath indexPath: NSIndexPath) -> UICollectionViewCell {
+        let cell = collectionView.dequeueReusableCellWithReuseIdentifier("Cell", forIndexPath: indexPath) as UICollectionViewCell
+        cell.layer.cornerRadius = cell.frame.size.width / 2.0
+        return cell
+    }
+    
+    func collectionView(collectionView: UICollectionView, viewForSupplementaryElementOfKind kind: String, atIndexPath indexPath: NSIndexPath) -> UICollectionReusableView {
+        let view = collectionView.dequeueReusableSupplementaryViewOfKind(kind, withReuseIdentifier: "Add", forIndexPath: indexPath) as UICollectionReusableView
+        return view
+    }
 
 }
 
